a926652893881bf19ffb1acee02d3d03e2a10df6
amirhossein <amirhosseinazimyzadeh2@gmail.com>
2019-10-28 21:52:31 +0330

    complete face and vertex class

--- src/artexCore/Face.java
+++ src/artexCore/Face.java
@@ -1,4 +1,53 @@
 package artexCore;
 
+import java.util.ArrayList;
+/**
+ * Face class create a page with it's vertices
+ * each faces or 3D Objects have one anchor , anchors used to rotate,
+ * resize, move and copy objects and faces
+ *
+ *
+ * @author Amirhossein Azimyzadeh
+ */
+
 public class Face {
+    private ArrayList<Vertex> vertices;
+    private Vertex anchor;
+
+    public Face(ArrayList<Vertex> vertices, Vertex anchor) {
+        this.vertices = vertices;
+        this.anchor = anchor;
+    }
+
+    public Face(ArrayList<Vertex> vertices) {
+        this.vertices = vertices;
+        if(vertices.size() != 0)
+            this.anchor = vertices.get(0);
+        else
+            this.anchor = new Vertex(0,0,0);
+    }
+
+    public ArrayList<Vertex> getVertices() {
+        return vertices;
+    }
+
+    public Vertex getVertex(int index){
+        if(vertices.size()!=0)
+            return vertices.get(index);
+        else
+            return null;
+    }
+
+    public void setVertices(ArrayList<Vertex> vertices) {
+        this.vertices = vertices;
+    }
+
+    public Vertex getAnchor() {
+        return anchor;
+    }
+
+    public void setAnchor(Vertex anchor) {
+        this.anchor = anchor;
+    }
+
 }
--- src/artexCore/Vertex.java
+++ src/artexCore/Vertex.java
@@ -1,4 +1,67 @@
 package artexCore;
 
+/**
+ * vertex id shows line number in final file and use for facing vertices
+ * @see Face
+ *
+ * @author Amirhossein Azimyzadeh
+ * */
+
 public class Vertex {
+    private float x;
+    private float y;
+    private float z;
+    private int id;
+
+    public Vertex(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vertex(float x, float y, float z, int id) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.id = id;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public int getId() {
+        return id;
+    }
+    /**
+     * only use when writing to .obj file or reading from file
+     * */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "v "+x+" "+y+" "+z;
+    }
 }
