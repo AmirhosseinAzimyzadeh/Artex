5777eca832ee506e66116735295a103369241704
amirhossein <amirhosseinazimyzadeh2@gmail.com>
2019-11-01 11:11:16 +0330

    create GeoMath class for handling mathematics in Artex

---
+++ src/utility/GeoMath.java
@@ -0,0 +1,29 @@
+package utility;
+
+import artexCore.Vertex;
+/**
+ * Geometric Math class created for mathematical utility in other classes
+ * @author Amirhossein Azimyzadeh
+ * */
+public class GeoMath {
+
+    public static Vertex[] regularPolygonVertices(int points, float radius){
+        Vertex[] result = new Vertex[points];
+        float[] degrees = new float[points];
+        float degreeAmount = (float) (2*Math.PI/points);
+        float sum=0;
+        for(int i = 0 ;i<degrees.length ;i++){
+            sum += degreeAmount;
+            degrees[i] = sum;
+        }
+        for (int i = 0; i < result.length; i++) {
+            float x = (float) (radius* Math.cos( degrees[i]));
+            float y = (float) (radius* Math.sin( degrees[i]));
+            float z = 0.0f;
+            Vertex v = new Vertex(x,y,z);
+            result[i] = v;
+        }
+        return result;
+    }
+
+}
