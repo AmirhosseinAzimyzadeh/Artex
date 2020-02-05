a6786b634ad4f76df1c5ed1e311b7106ff262358
Amirhossein Azimyzadeh <39561409+amirhosseinazimyzadeh@users.noreply.github.com>
2020-02-05 16:38:13 +0330

    add general rotate method

--- src/interfaces/Rotate.java
+++ src/interfaces/Rotate.java
@@ -2,6 +2,10 @@ package interfaces;
 
 public interface Rotate<T> {
     T rotateX(float degree);
+
     T rotateY(float degree);
+
     T rotateZ(float degree);
+
+    T rotate(float X, float Y, float Z);
 }
