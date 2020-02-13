5f8a72d8c99011f3117f7d3e21e64c20672f4ebf
Amirhossein Azimyzadeh <39561409+amirhosseinazimyzadeh@users.noreply.github.com>
2020-02-13 18:35:38 +0330

    sphere first init

--- src/artexObjects/Sphere.java
+++ src/artexObjects/Sphere.java
@@ -1,4 +1,21 @@
 package artexObjects;
 
+import artexCore.Face;
+import artexCore.Vertex;
+/**
+ * @see Face
+ * @see utils.GeoMath
+ * @author Amirhossein Azimyzadeh
+ * */
 public class Sphere {
+    private Face[] faces;
+    private int segments;
+
+    public Sphere(int segments, float radius){
+
+    }
+
+    public ComplexObject convertToComplexObject(){
+        return new ComplexObject(this.faces);
+    }
 }
